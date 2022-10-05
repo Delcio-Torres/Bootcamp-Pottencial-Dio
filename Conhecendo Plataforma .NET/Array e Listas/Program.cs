dcf4c32d75bd731ada7c8a51038f17bccfc36300
delcio-torres <delcio.at@gmail.com>
2022-10-04 23:22:17 -0300

    Trabalhando com Array e List

---
+++ Conhecendo Plataforma .NET/Array e Listas/Program.cs
@@ -0,0 +1,51 @@
+﻿
+// TRABALHANDO COM LISTA ---------------------------------------------
+
+List<string> listaString = new List<string>();
+
+listaString.Add("Delcio");
+listaString.Add("Olivia");
+listaString.Add("Gabriel");
+listaString.Add("Raphael");
+
+for( int contador = 0; contador < listaString.Count; contador++){
+
+    Console.WriteLine($"Minha família linda {contador+1} : {listaString[contador]}");
+
+}
+
+foreach(string item in listaString){
+    Console.WriteLine($"Minha família linda: {item}");
+}
+
+
+// TRABALHANDO COM ARRAY --------------------------------------------------
+
+// int[] arrayInteiros = new int[3];
+
+// arrayInteiros[0] = 72;
+// arrayInteiros[1] = 64;
+// arrayInteiros[2] = 50;
+
+// Console.WriteLine("Lista com For");
+
+// for(int contador = 0; contador < arrayInteiros.Length; contador++){
+//     Console.WriteLine($"Posição nº {contador} = {arrayInteiros[contador]}");
+// }
+
+// Console.WriteLine();
+
+// Console.WriteLine("Lista com Foreach");
+// foreach(int valor in arrayInteiros){
+//     Console.WriteLine(valor);
+// }
+// Array.
+// Console.WriteLine();
+
+// Console.WriteLine("Tamanho do meu array: " + arrayInteiros.Length);
+
+// Array.Resize(ref arrayInteiros, 10);
+
+// Console.WriteLine();
+
+// Console.WriteLine("Tamanho do meu novo array: " + arrayInteiros.Length);
